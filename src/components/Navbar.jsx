210867d07c22a1eb3b43f63f1123eca9ee9b1b1c
mbarto202 <michael.d.barto@gmail.com>
2023-05-29 11:16:51 -0400

    fixed positioning issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,8 @@ import './navbar.css'
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <div className='container'>
-        <div className="logo">logo</div>
-        <div className="link">link</div>
-      </div>
+      <div className="logo">logo</div>
+      <div className="links">links</div>
     </div>
   )
 }
