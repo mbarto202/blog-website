470b3110e93d5d87075719e8d8fbbe2d8eff9e8d
mbarto202 <michael.d.barto@gmail.com>
2023-05-30 11:41:21 -0400

    home nav and logo need to be repositioned

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './navbar.css'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <div className="logo">logo</div>
-      <div className="links">links</div>
+      <div className="logo">Barto Blog</div>
+      <div className="links">
+        <Link className='link'>Links</Link>
+      </div>
     </div>
   )
 }
