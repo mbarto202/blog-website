89a8d891f7743d201b553ae963c9a2f76e5e4da5
mbarto202 <michael.d.barto@gmail.com>
2023-09-14 11:56:58 -0400

    single

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -3,6 +3,17 @@ import './single.css'
 import { Link } from 'react-router-dom'
 // /post/1
 // change post num on edit link
+const posts = [
+  {
+    id: 1,
+    title: "pomp",
+    desc: "pomp"},
+
+    {
+      id: 1,
+      title: "pomp",
+      desc: "pomp"}
+];
 const Single = () => {
   return (
     <div className="single">
@@ -17,10 +28,19 @@ const Single = () => {
           <Link to={'/write?edit=2'}>delete</Link>
         </div>
         <h1>Title</h1>
-        <p>hello my name is michael and this is a placeholder for the content that will be in the post. <br />this website will represent my knowledge of javascript html/css and mysql</p>
+        <p className='desc'>hello my name is michael and this is a placeholder for the content that will be in the post. <br />this website will represent my knowledge of javascript html/css and mysql</p>
       </div>
       
-      <div className="menu">Menu</div>
+      <div className="menu">
+        <h1>Other posts you may like</h1>
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <img src={post.img} alt=""/>
+            <h2>{post.title}</h2>
+            <button>Read More</button>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
