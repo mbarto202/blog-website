19bd1dedb8d88fe15af9a9a3a96bb3c691fec1cb
mbarto202 <michael.d.barto@gmail.com>
2023-12-16 15:09:59 -0500

    api

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,5 @@
 import {db} from "../db.js";
+import bcrypt from "bcryptjs";
 
 export const register = (req, res)=>{
 
@@ -8,6 +9,22 @@ const q = "SELECT * FROM users WHERE email = ? OR username = ?";
 db.query(1,[req.body.email, req.body.username], (err,data)=>{
     if(err) return res.json(err);
     if (data.length) return res.status(409).json("User already exists!");
+
+    //Create user & hash pass
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync("req.body.password", salt);
+
+    const q = "INSERT INTO users('username', 'email', 'password') VALUES (?)"
+    const values = [
+        req.body.username,
+        req.body.email,
+        hash,
+    ]
+
+    db.query(q[values], (err,data)=>{
+        if(err) return res.json(err);
+        return res.status(200).json("User has be created.");
+    })
 });
 
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import './login.css'
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username:"",
+    email:"",
+    password:"",
+  })
+
+  const handleChange = e =>{
+    setInputs(prev=>({...prev,[e.target.name]: e.target.value}))
+  }
   return (
     <div className='auth'>
         <h1>Register</h1>
         <form>
-          <input required type="text" placeholder='Username' />
-          <input required type="email" placeholder='Email' />
-          <input required type="password" placeholder='Password' />
+          <input required type="text" placeholder='username' name='username' onChange={handleChange} />
+          <input required type="email" placeholder='email' name='email' onChange={handleChange}/>
+          <input required type="password" placeholder='password' name='password' onChange={handleChange}/>
           <button>Register</button>
           <p>Error</p>
           <span>Already have an account? <a href="/login">Login</a></span>
