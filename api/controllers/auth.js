cd575fcc5f05fa8d3bafa07f69177ebcd226ee89
mbarto202 <michael.d.barto@gmail.com>
2023-12-17 16:04:27 -0500

    solving db issue

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -6,15 +6,15 @@ export const register = (req, res)=>{
 //check existing user
 const q = "SELECT * FROM users WHERE email = ? OR username = ?";
 
-db.query(1,[req.body.email, req.body.username], (err,data)=>{
+db.query(q, [req.body.email, req.body.username], (err,data)=>{
     if(err) return res.json(err);
     if (data.length) return res.status(409).json("User already exists!");
 
     //Create user & hash pass
     const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync("req.body.password", salt);
+    const hash = bcrypt.hashSync(req.body.password, salt);
 
-    const q = "INSERT INTO users('username', 'email', 'password') VALUES (?)"
+    const q = "INSERT INTO users('username','email','password') VALUES (?)"
     const values = [
         req.body.username,
         req.body.email,
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './login.css'
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 const Register = () => {
   const [inputs, setInputs] = useState({
@@ -11,6 +13,15 @@ const Register = () => {
   const handleChange = e =>{
     setInputs(prev=>({...prev,[e.target.name]: e.target.value}))
   }
+
+  const handleSubmit = async e =>{
+    e.preventDefault()
+    try{
+    const res = await axios.post("/auth/register", inputs)
+    console.log(res)
+    console.log('AHHHHHHHHHHH')}
+    catch(err){console.log(err)}
+  }
   return (
     <div className='auth'>
         <h1>Register</h1>
@@ -18,7 +29,7 @@ const Register = () => {
           <input required type="text" placeholder='username' name='username' onChange={handleChange} />
           <input required type="email" placeholder='email' name='email' onChange={handleChange}/>
           <input required type="password" placeholder='password' name='password' onChange={handleChange}/>
-          <button>Register</button>
+          <button onClick={handleSubmit}>Register</button>
           <p>Error</p>
           <span>Already have an account? <a href="/login">Login</a></span>
         </form>
